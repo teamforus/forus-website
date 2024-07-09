890bbab2522ebf6449b0606716c5b0fb95f17a1d
Aleksei Loginov <aleksei@forus.io>
2024-07-09 16:20:23 +0200

    added missed quotes

--- react/src/dashboard/components/elements/tables/EventLogsTable.tsx
+++ react/src/dashboard/components/elements/tables/EventLogsTable.tsx
@@ -56,7 +56,7 @@ export default function EventLogsTable({
             { key: 'fund', title: 'Fonds' },
             { key: 'employees', title: 'Medewerker' },
             { key: 'bank_connection', title: 'Bank integratie' },
-            { key: 'voucher', title: Tegoeden },
+            { key: 'voucher', title: 'Tegoeden' },
         ].filter((item) => hasPermission(organization, permissionsMap[item.key]));
     }, [organization, permissionsMap]);
 
