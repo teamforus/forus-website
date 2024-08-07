66488ea9df5d21bd372417f3083cb9bdf800b6de
dev-rminds <dev@rminds.nl>
2024-08-07 19:39:45 +0300

    scrollable tables - hide top scroll when bottom scroller is visible

--- react/src/dashboard/components/elements/tables/TableTopScroller.tsx
+++ react/src/dashboard/components/elements/tables/TableTopScroller.tsx
@@ -1,50 +1,89 @@
-import React, { ReactElement, useCallback, useEffect, useRef } from 'react';
+import React, { ReactElement, useCallback, useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 
-export default function TableTopScroller({ children }: { children: ReactElement | ReactElement[] }) {
-    const tableScrollWrapperRef = useRef<HTMLDivElement>(null);
+export default function TableTopScroller({
+    children,
+    scrollableRootSelector = '.app',
+}: {
+    children: ReactElement | ReactElement[];
+    scrollableRootSelector?: string;
+}) {
     const tableScrollTopRef = useRef<HTMLDivElement>(null);
+    const tableScrollWrapperRef = useRef<HTMLDivElement>(null);
     const tableScrollTopContentRef = useRef<HTMLDivElement>(null);
 
+    const [showTopScroll, setShowTopScroll] = useState(false);
+
     const handleResize = useCallback((enabled = true) => {
-        if (enabled && tableScrollWrapperRef.current) {
+        if (enabled && tableScrollTopRef.current && tableScrollWrapperRef.current) {
             tableScrollTopRef.current.style.width = `${tableScrollWrapperRef.current.offsetWidth}px`;
             tableScrollTopContentRef.current.style.width = `${tableScrollWrapperRef.current.scrollWidth}px`;
             tableScrollTopContentRef.current.style.height = `1px`;
         }
     }, []);
 
-    useEffect(() => {
-        // Initial width and scrollWidth set
+    const handlePositionChange = useCallback(() => {
+        const rect = tableScrollWrapperRef?.current?.getBoundingClientRect();
+        const screenHeight = window.innerHeight;
+
+        setShowTopScroll(rect && rect?.bottom > screenHeight);
+    }, []);
+
+    const updateState = useCallback(() => {
         handleResize();
+        handlePositionChange();
+    }, [handlePositionChange, handleResize]);
 
-        window.addEventListener('resize', handleResize);
+    useEffect(() => {
+        const appRoot = document.querySelector(scrollableRootSelector);
 
-        const observer = new MutationObserver(handleResize);
+        if (!appRoot) {
+            return;
+        }
+
+        updateState();
+
+        window.addEventListener('resize', updateState);
+        appRoot.addEventListener('scroll', updateState);
+
+        const observer = new MutationObserver(updateState);
 
         if (tableScrollWrapperRef.current) {
-            observer.observe(tableScrollWrapperRef.current, { childList: true, subtree: true, characterData: true });
+            const options = {
+                childList: true,
+                subtree: true,
+                characterData: true,
+            };
+
+            observer.observe(tableScrollWrapperRef.current, options);
+            observer.observe(appRoot, options);
+            observer.observe(document.body, options);
         }
 
         // Cleanup event listener and observer on component unmount
         return () => {
-            window.removeEventListener('resize', handleResize);
             observer.disconnect();
+            window.removeEventListener('resize', updateState);
+            appRoot.removeEventListener('scroll', updateState);
         };
-    }, [handleResize]);
+    }, [scrollableRootSelector, updateState]);
 
     return (
         <div className="table-wrapper">
-            <div className="table-container">
-                <div
-                    className="table-scroll-top"
-                    ref={tableScrollTopRef}
-                    onScroll={(e) => tableScrollWrapperRef.current.scrollTo({ left: e.currentTarget.scrollLeft })}>
-                    <div className="table-scroll-top-content" ref={tableScrollTopContentRef} />
-                </div>
+            <div className={classNames('table-container', showTopScroll && 'table-container-scroll-top')}>
+                {showTopScroll && (
+                    <div
+                        className="table-scroll-top"
+                        ref={tableScrollTopRef}
+                        onScroll={(e) => tableScrollWrapperRef.current?.scrollTo({ left: e.currentTarget.scrollLeft })}>
+                        <div className="table-scroll-top-content" ref={tableScrollTopContentRef} />
+                    </div>
+                )}
+
                 <div
                     className="table-scroll"
                     ref={tableScrollWrapperRef}
-                    onScroll={(e) => tableScrollTopRef.current.scrollTo({ left: e.currentTarget.scrollLeft })}>
+                    onScroll={(e) => tableScrollTopRef.current?.scrollTo({ left: e.currentTarget.scrollLeft })}>
                     {children}
                 </div>
             </div>
